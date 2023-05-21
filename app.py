4731100006cd8af88d7b0cffc252fc8222db4c77
andi598610 <andi598610@gmail.com>
2023-05-21 23:24:09 +0300

    updated with nlp

--- app.py
+++ app.py
@@ -1,37 +1,49 @@
 import streamlit as st
 import pandas as pd
+import spacy
+from sklearn.metrics.pairwise import cosine_similarity
+from spacy.lang.en.stop_words import STOP_WORDS
 
-uploaded_file = st.sidebar.file_uploader('Upload your Talent CSV')
-if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
+# Load the Spacy model with pre-trained word vectors
+nlp = spacy.load('en_core_web_md')
 
 def main():
-    st.title('Search for your star')
+    uploaded_file = st.sidebar.file_uploader('Upload your Talent CSV')
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
 
-    search_term = st.text_input('Enter attributes')
+        # Concatenate all columns
+        df['all_text'] = df.apply(lambda x: ' '.join(x.astype(str)), axis=1)
 
-    if search_term:
-        # Split the search term into individual words
-        search_terms = search_term.split()
+        # Generate word vectors for all_text column
+        df['vector'] = df['all_text'].apply(lambda x: nlp(x).vector)
 
-        # Initialize a mask to select all rows
-        mask = pd.Series([True] * len(df))
+        st.title('Search for your star')
 
-        for term in search_terms:
-            # Case-insensitive search in all String columns
-            string_cols = df.select_dtypes(include='object')
-            term_mask = string_cols.apply(lambda x: x.str.contains(term, case=False, na=False)).any(axis=1)
+        search_term = st.text_input('Enter attributes')
 
-            # Case-insensitive search in all numeric columns
-            numeric_cols = df.select_dtypes(include='number')
-            term_mask |= numeric_cols.apply(lambda x: x == pd.to_numeric(term, errors='coerce')).any(axis=1)
-            
-            # Combine the mask for this term with the overall mask
-            mask &= term_mask
+        if search_term:
+            # Ignore stop words in search terms
+            search_terms = [word for word in search_term.split() if word not in STOP_WORDS]
+            search_term = ' '.join(search_terms)
+            search_vector = nlp(search_term).vector
+            df['similarity'] = df['vector'].apply(lambda x: cosine_similarity([x], [search_vector])[0][0])
 
-        results = df[mask]
-        
-        st.write(results)
+            # Generate masks for each search term
+            masks = [df['all_text'].str.contains(rf"\b{term}\b", case=False, na=False, regex=True) for term in search_terms]
+
+            # Combine the masks
+            mask = masks[0]
+            for m in masks[1:]:
+                mask &= m
+
+            # Only consider entries where all search terms were found
+            df_filtered = df[mask]
+
+            # Get top 100 most similar entries
+            results = df_filtered.nlargest(100, 'similarity')
+
+            st.write(results)
 
 if __name__ == "__main__":
     main()
